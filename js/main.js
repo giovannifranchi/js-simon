078f9b5129600a00432563ca8d8460e1fc29a857
giovannifranchi <giovanni.franchi95@gmail.com>
2023-03-06 18:35:38 +0100

    Feat: timout functions and game logic

--- js/main.js
+++ js/main.js
@@ -40,6 +40,27 @@ function hideElements(arrayOfElements, classHide){
     }
 }
 
+function askNumbers(times){
+    const arrayOfAskedNums = [];
+    for( let i = 0; i < times; i++){
+        const askedNum = Number(prompt('eneter a number', ''));
+        arrayOfAskedNums.push(askedNum);
+    }
+    return arrayOfAskedNums;
+}
+
+function controlCoincidence(list, controlList){
+    const coincidentNums = [];
+    for(let i = 0; i < list.length; i++){
+        if(controlList.includes(list[i])){
+            coincidentNums.push(list[i]);
+        }
+    }
+    return coincidentNums;
+}
+
+
+
 const container = '.container'
 const classNameNumbers = 'box';
 const arrayOfElements = '.box';
@@ -49,6 +70,8 @@ const lengthOfNumbers = 5;
 const element = 'div';
 const classHide = 'hidden';
 const randomArray = createRandomArray(lengthOfNumbers, minNum, maxNum);
+let arrayOfAskedNums = [];
+let coincidentNums = [];
 
 createHTMLElement(element, randomArray.length, container, classNameNumbers);
 fillInnerText(arrayOfElements, randomArray);
@@ -56,4 +79,17 @@ fillInnerText(arrayOfElements, randomArray);
 let hide = setTimeout(()=>{
     hideElements(arrayOfElements, classHide);
 }, 4000);
+
+let CallarrayOfAskedNums = setTimeout(()=>{
+    arrayOfAskedNums = [...askNumbers(lengthOfNumbers)];
+}, 5000);
+
+let callCoincidentArray =  setTimeout(()=>{
+    coincidentNums = [...controlCoincidence(arrayOfAskedNums, randomArray)];
+}, 6000);
+
+let callResult = setTimeout(()=>{
+    createHTMLElement(element, coincidentNums.length, container, classNameNumbers);
+}, 7000);
+
 
